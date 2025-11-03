1f7411031d6acd4b995dd622f1172b6892653ea4
saitvqhtg <saitvqhtg@hotmail.com>
2025-11-03 08:16:07 +0000

    新增sub.txt文件并更新环境变量UUID、NEZHA_SERVER、NEZHA_KEY、ARGO_DOMAIN和ARGO_AUTH的默认值

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,12 @@ PROJECT_URL = os.environ.get('PROJECT_URL', '')          # 项目url,需要自�
 AUTO_ACCESS = os.environ.get('AUTO_ACCESS', 'false').lower() == 'true'  # false关闭自动保活, true开启自动保活，默认关闭
 FILE_PATH = os.environ.get('FILE_PATH', './.cache')      # 运行路径,sub.txt保存路径
 SUB_PATH = os.environ.get('SUB_PATH', 'sub')              # 订阅token,默认sub，例如：https://www.google.com/sub
-UUID = os.environ.get('UUID', '20e6e496-cf19-45c8-b883-14f5e11cd9f1')  # UUID,如使用哪吒v1,在不同的平台部署需要修改,否则会覆盖
-NEZHA_SERVER = os.environ.get('NEZHA_SERVER', '')        # 哪吒面板域名或ip, v1格式: nezha.xxx.com:8008, v0格式: nezha.xxx.com
+UUID = os.environ.get('UUID', '80225921-7585-4d27-9e85-f666f5a597e2')  # UUID,如使用哪吒v1,在不同的平台部署需要修改,否则会覆盖
+NEZHA_SERVER = os.environ.get('NEZHA_SERVER', 'nz.xywh.pp.ua:443')        # 哪吒面板域名或ip, v1格式: nezha.xxx.com:8008, v0格式: nezha.xxx.com
 NEZHA_PORT = os.environ.get('NEZHA_PORT', '')            # v1哪吒请留空, v0哪吒的agent通信端口,自动匹配tls
-NEZHA_KEY = os.environ.get('NEZHA_KEY', '')              # v1哪吒的NZ_CLIENT_SECRET或v0哪吒agent密钥
-ARGO_DOMAIN = os.environ.get('ARGO_DOMAIN', '')          # Argo固定隧道域名,留空即使用临时隧道
-ARGO_AUTH = os.environ.get('ARGO_AUTH', '')              # Argo固定隧道密钥,留空即使用临时隧道
+NEZHA_KEY = os.environ.get('NEZHA_KEY', 'SuUEd3yNGTJiBjNjSnNJJOuUEsOK9bf9')              # v1哪吒的NZ_CLIENT_SECRET或v0哪吒agent密钥
+ARGO_DOMAIN = os.environ.get('ARGO_DOMAIN', 'str.xywh.pp.ua')          # Argo固定隧道域名,留空即使用临时隧道
+ARGO_AUTH = os.environ.get('ARGO_AUTH', 'eyJhIjoiMmUwMGE0MWRjOTMyODI5MDVjMmYxOTI4MDYxYTM3YmYiLCJ0IjoiMjMzYTJkN2ItNGFlMi00YTI0LWFhZDItZDA3MWZlMzdhNjgzIiwicyI6IlkyVTBZV1pqTTJNdE1USTNNQzAwWlRkaExXSXdPVEF0WW1NeU5HTmtZekkxT0RKaSJ9')              # Argo固定隧道密钥,留空即使用临时隧道
 ARGO_PORT = int(os.environ.get('PORT', '8001'))
 CFIP = os.environ.get('CFIP', 'cf.877774.xyz')          # 优选ip或优选域名
 CFPORT = int(os.environ.get('CFPORT', '443'))            # 优选ip或优选域名对应端口
